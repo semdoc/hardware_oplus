cd3cd9aaf33aef8e18390709aa0cb6823bc616b8
AnierinB <anierin@evolution-x.org>
2023-01-25 21:32:16 +0300

    hw_oplus: OplusParts: fix Search in Settings
    Change-Id: Idf18439dae63d1188244f66678d20db591ba12c2
    Signed-off-by: Semavi Ulusoy <doc.divxm@gmail.com>

--- OplusParts/src/com/aicp/oplus/OplusParts/OplusPartsActivity.java
+++ OplusParts/src/com/aicp/oplus/OplusParts/OplusPartsActivity.java
@@ -1,31 +1,13 @@
 /*
- * Copyright (C) 2018-2022 crDroid Android Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright (C) 2017 The OmniROM Project
+ *               2022 The Evolution X Project
+ * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
 package com.aicp.oplus.OplusParts;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
 import android.app.Fragment;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import androidx.preference.PreferenceFragment;
 import androidx.preference.PreferenceManager;
 
@@ -34,13 +16,20 @@ import com.android.settingslib.widget.R;
 
 public class OplusPartsActivity extends CollapsingToolbarBaseActivity {
 
-    private static final String TAG = "OplusParts";
+    private OplusParts mOplusPartsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager().beginTransaction().replace(R.id.content_frame,
-                new OplusParts(), TAG).commit();
+        Fragment fragment = getFragmentManager().findFragmentById(R.id.content_frame);
+        if (fragment == null) {
+            mOplusPartsFragment = new OplusParts();
+            getFragmentManager().beginTransaction()
+                .add(R.id.content_frame, mOplusPartsFragment)
+                .commit();
+        } else {
+            mOplusPartsFragment = (OplusParts) fragment;
+        }
     }
 }
